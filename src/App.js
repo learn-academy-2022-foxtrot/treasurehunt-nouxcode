be49d39f2720c00d9b38dd2b7201415fe5940a4c
nouxcode <perrault.meaghan@gmail.com>
2022-09-26 11:36:22 -0700

    modified components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
-import Square from "./components/Square"
-import "./App.css"
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useState } from "react";
+import "./App.css";
+import Square from "./components/Square";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -12,19 +13,61 @@ const App = () => {
     "?",
     "?",
     "?",
-    "?"
-  ])
+    "?",
+  ]);
 
+  const [treasureLocation, setTreasureLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+  const [bombLocation, setBombLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+
+  const handleGamePlay = (clickedSquare) => {
+    // makes a copy of the board in state so that we can modify it in this method
+    let updateBoard = [...board];
+    if (clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "👑";
+      setBoard(updateBoard);
+    } else if (clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣";
+      setBoard(updateBoard);
+    } else {
+      updateBoard[clickedSquare] = "🌳";
+      setBoard(updateBoard);
+    }
+  };
+
+  const handleReset = () => {
+    setBoard(["?", "?", "?", "?", "?", "?", "?", "?", "?"]);
+    setTreasureLocation(Math.floor(Math.random() * board.length));
+    setBombLocation(Math.floor(Math.random() * board.length));
+  };
+
+  console.log(
+    "treasureLocation:",
+    treasureLocation,
+    "bombLocation:",
+    bombLocation
+  );
   return (
     <>
       <h1>Treasure Hunt Game</h1>
-      <div className="board-game">
+      <div className="game_container">
         {board.map((square, index) => {
-          return <Square square={square} />
+          return (
+            <Square
+              square={square}
+              index={index}
+              key={index}
+              handleGamePlay={handleGamePlay}
+            />
+          );
         })}
       </div>
+      <button onClick={handleReset}>Restart Game</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
